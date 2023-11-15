c23949a494af89e83e48f139c319c6087f7a373a
Giorgi Bliadze <bliadze1997@gmail.com>
2023-11-16 02:38:59 +0400

    SearchManufacturer

---
+++ components/CustomFilter.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CustomFilter = ({ title }) => {
+  return <div>{title}</div>;
+};
+
+export default CustomFilter;
--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,15 @@ import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import CustomButton from "./CustomButton";
-
-export { Hero, Navbar, Footer, CustomButton };
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+export {
+  Hero,
+  Navbar,
+  Footer,
+  CustomButton,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+};
