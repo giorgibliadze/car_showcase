53c0749a12acb54f36e0d92b10d319fed496b15f
Giorgi Bliadze <bliadze1997@gmail.com>
2023-11-17 13:56:10 +0400

    CarCard first step

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
 
-const CarCard = ({ car }) => {
-  return <div>CarCard</div>;
+import { useState } from "react";
+import Image from "next/image";
+import { CarProps } from "@/types";
+import { CustomButton } from ".";
+interface CarCardProps {
+  car: CarProps;
+}
+
+const CarCard = ({ car }: CarCardProps) => {
+  const { city_mpg, year, make, model, transmission, drive } = car;
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        <h2 className="car-card__content-title">
+          {make}
+          {model}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
